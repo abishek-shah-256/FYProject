31a96a882c2b1d1535d38b95d5c78a1c5765e1d6
abishek-shah-256 <shahabishek63@gmail.com>
2023-04-25 12:18:22 +0545

    uploading project

---
+++ finwold/app1/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from app1.models import User, CompanyName, Expense, Category, Contact_Us
+
+admin.site.register(User)
+admin.site.register(Contact_Us)
+admin.site.register(CompanyName)
+admin.site.register(Expense)
+admin.site.register(Category)
